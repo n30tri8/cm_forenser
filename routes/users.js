53633deb263f54a83e1db92add5cc41c80807bcf
Pooya Torabi <pooyatorabi76@gmail.com>
2020-07-03 22:22:37 +0430

    added express-user-management module

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+  console.log(req.body)
   res.send('respond with a resource');
 });
 
